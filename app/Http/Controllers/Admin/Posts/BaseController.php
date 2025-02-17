744062b0c8c067f6929523797be0b451a124bec4
cellphonezzz <lilqola@yandex.ru>
2025-02-17 22:24:07 +0700

    adding database links. adding crud by users

---
+++ app/Http/Controllers/Admin/Posts/BaseController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Posts;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class CreateController extends Controller
+{
+    public function __invoke()
+    {
+        $categories = Category::all();
+
+        $tags = Tag::all();
+
+        return view('admin.posts.create', compact('categories', 'tags'));
+    }
+}
---
+++ app/Http/Controllers/Admin/Users/StoreController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Categories;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Categories\StoreRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    public function __invoke(StoreRequest $request)
+    {
+        $data = $request->validated();
+        Category::create($data);
+        return redirect()->route('admin.category.index');
+    }
+}
